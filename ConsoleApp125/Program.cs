794d906c8d34895686f316b46b1b9bf7ebdcf9c3
nastyagurskaya <just.gurskaya@gmail.com>
2018-07-20 18:54:43 +0300

    Add project files.

---
+++ ConsoleApp125/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ConsoleApp125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commandToExec = "calc";
+            ProcessService service = new ProcessService();
+            Console.WriteLine($@"Starting Actioner with ""command"" param: {commandToExec}");
+            var actionResult = new Actioner(service).ExecuteCommand(commandToExec);
+            Console.WriteLine($"Action Result: {actionResult}");
+            Console.ReadKey();
+        }
+    }
+
+    public class Actioner
+    {
+        private readonly IProcessService _proc;
+        public Actioner(IProcessService proc)
+        {
+            _proc = proc;
+        }
+        public string ExecuteCommand(string command)
+        {
+            bool started = _proc.Start(command);
+            if (started) return "Command excuted";
+            else return "Executing failed";
+            //var procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command)
+            //{
+            //    RedirectStandardOutput = true,
+            //    RedirectStandardError = true,
+            //    UseShellExecute = false,
+            //    CreateNoWindow = true
+            //};
+
+            //var proc = new System.Diagnostics.Process {StartInfo = procStartInfo};
+            //proc.Start();
+            //var result = proc.StandardOutput.ReadToEnd();
+            //var resultError = proc.StandardError.ReadToEnd();
+            //var error = proc.ExitCode;
+
+            //if (error != 0)
+            //{
+            //    throw new Exception(result + resultError);
+            //}
+
+            //return result + resultError;
+        }
+    }
+    public interface IProcessService
+    {
+        bool Start(string command);
+    }
+    public class ProcessService : IProcessService
+    {
+        public bool Start(string command)
+        {
+            var procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command)
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            var proc = new System.Diagnostics.Process { StartInfo = procStartInfo };
+            proc.Start();
+            var result = proc.StandardOutput.ReadToEnd();
+            var resultError = proc.StandardError.ReadToEnd();
+
+            var error = proc.ExitCode;
+
+            if (error != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp125Tests/Actioner_Should.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp125;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleApp125Tests
+{
+    public class Actioner_Should
+    {
+        private readonly Actioner _commandClass;
+        private readonly IProcessService _service;
+
+        public Actioner_Should()
+        {
+            _service = Substitute.For<IProcessService>();
+            _commandClass = Substitute.For<Actioner>(_service);
+        }
+        [Fact(DisplayName = "Call Correct Execute Command")]
+        public void ServiceReceived()
+        {
+            _commandClass.ExecuteCommand("calc");
+            _service.Received().Start("calc");
+        }
+        [Theory]
+        [InlineData("calc", "Command excuted")]
+        [InlineData("", "Command excuted")]
+        [InlineData("notepad", "Command excuted")]
+        public void Call_CorrectExecuteCommand(string command, string expected)
+        {
+            //_commandClass.ExecuteCommand(Arg.Any<string>());
+            _service.Start(command).Returns(true);
+            string result = _commandClass.ExecuteCommand(command);
+            Assert.Equal(result, expected);
+        }
+        [Theory]
+        [InlineData("fhgtbnhgjn", "Executing failed")]
+        [InlineData("note", "Executing failed")]
+        public void Call_ThrowsExecuteCommand(string command, string expected)
+        {
+            //_commandClass.ExecuteCommand(Arg.Any<string>());
+            _service.Start(command).Returns(false);
+            string result = _commandClass.ExecuteCommand(command);
+            Assert.Equal(result, expected);
+        }
+        //[Theory]
+        //[InlineData("fhgtbnhgjn")]
+        //[InlineData("note")]
+        //public void Call_ThrowsExecuteCommand(string command)
+        //{
+        //    //_commandClass.ExecuteCommand(Arg.Any<string>());
+        //    Assert.Throws<Exception>(() => _commandClass.ExecuteCommand(command));
+        //}
+    }
+}
